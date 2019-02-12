782dcacf30bab22c640067098922f0466cf6fc4a
luojingyu <whaleluo@163.com>
2019-02-13 00:24:36 +0800

    使用Runnable 异步处理REST服务

--- whale-security-demo/src/main/java/com/whale/web/async/AsyncComtroller.java
+++ whale-security-demo/src/main/java/com/whale/web/async/AsyncComtroller.java
@@ -5,18 +5,37 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.Callable;
+
 @RestController
 public class AsyncComtroller{
 
     private Logger logger = LoggerFactory.getLogger(getClass());
 
-
-    @RequestMapping("/order")
+    /*@RequestMapping("/order")
     public String order() throws InterruptedException {
         //同步处理的方式
         logger.info("主线程开始");
         Thread.sleep(1000);
         logger.info("主线程返回");
         return "success";
+    }*/
+
+    @RequestMapping("/order")
+    public Callable<String>  order() throws InterruptedException {
+        logger.info("主线程开始");
+
+        Callable<String> result = new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                logger.info("副线程开始");
+                Thread.sleep(1000);
+                logger.info("副线程返回");
+                return "success";
+            }
+        };
+        Thread.sleep(1000);
+        logger.info("主线程返回");
+        return result;
     }
 }
